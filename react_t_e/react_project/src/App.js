f1c4a2353d670854b461bbcffafa993dbafd9950
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-11-08 15:16:10 +0400

    environment variables

--- react_t_e/react_project/src/App.js
+++ react_t_e/react_project/src/App.js
@@ -1,6 +1,20 @@
-
+import logo from "./logo.svg";
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <img src="/logo192.png" />
+          <img src={logo} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
