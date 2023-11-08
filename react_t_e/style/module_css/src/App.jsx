8af7e7244a04ca4690dc46a886786233ce17ee35
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-11-08 18:00:43 +0400

    create page with css modules

--- react_t_e/style/module_css/src/App.jsx
+++ react_t_e/style/module_css/src/App.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import styles from "./App.module.css";
+import Components from "./Components";
 function App() {
-  return <>TEST</>;
+  useEffect(() => {
+    console.log(styles);
+  }, []);
+  return (
+    <div className={styles.App}>
+      <h1>First page</h1>
+      <Components className="App" />
+    </div>
+  );
 }
 
 export default App;
---
+++ react_t_e/style/module_css/src/Components.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Components = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/project">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Components;
