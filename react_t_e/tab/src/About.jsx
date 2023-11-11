6fe29da833e259e9bfb8bb5281c3accb21ffe73d
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-11-11 18:57:52 +0400

    add some changes

--- react_t_e/tab/src/About.jsx
+++ react_t_e/tab/src/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Work with us
+              Work with 
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
